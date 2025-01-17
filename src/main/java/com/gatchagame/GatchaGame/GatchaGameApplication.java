781d9a87aaaf5f7f09d41ff7adf2e9e2428b81d0
Amaniesaid <saidamanie2002@gmail.com>
2025-01-17 14:42:41 +0100

    Commit projet

---
+++ src/main/java/com/gatchagame/GatchaGame/GatchaGameApplication.java
@@ -0,0 +1,13 @@
+package com.gatchagame.GatchaGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatchaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatchaGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gatchagame/GatchaGame/GatchaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatchagame.GatchaGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatchaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
